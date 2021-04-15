285c91ef367d61a07598beb0801dec0780c3cdae
Abrocoly <tomimoreno03@gmail.com>
2021-04-15 01:21:09 -0300

    Reformando estilos del login

--- js/captcha.js
+++ js/captcha.js
@@ -1,19 +1,11 @@
- var validado=false;
-
+const boton = document.getElementById("ingreso");
 
 //AL DETECTAR MOVIMIENTO DEL MOUSE, HABILITA LA VALIDACIÓN
 function deshabilitar(){
-        var boton = document.getElementById("ingreso");
-        console.log("hola");
         boton.disabled = true;
-
 }
 
 function validar(){
-
-        var boton = document.getElementById("ingreso");
-        console.log("chau");
         boton.disabled = false;
-
 }
 
